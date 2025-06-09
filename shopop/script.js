f00f9b21b695e7f8e2b691d88119730d8d803136
moni-sha17 <monisha1852005@gmail.com>
2025-06-09 11:31:46 +0530

    Add files via upload

---
+++ shopop/script.js
@@ -0,0 +1,8 @@
+var sidenav = document.querySelector(".side-navbar")
+ function slide(){
+    sidenav.style.transform="translateX(0)"
+ }
+
+ function closeslide(){
+    sidenav.style.transform="translateX(-100%)"
+ }
